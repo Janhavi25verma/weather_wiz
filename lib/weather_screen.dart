117bff642fcd5ac9ac4d0e5bb9fb9c8e44a6622e
Janhavi25verma <janhavi25verma@gmail.com>
2023-10-25 22:47:39 +0530

    Main card and scrollable cards

---
+++ lib/weather_screen.dart
@@ -0,0 +1,164 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          // GestureDetector(child: const Icon(Icons.refresh)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //MAIN CARD
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 20,
+                      sigmaY: 20,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          Text(
+                            "300°K",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 30,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Rain",
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //HEADING2
+            const Text(
+              "Weather Forecast",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            //SMALL CARD
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyWeatherCard(),
+                  HourlyWeatherCard(),
+                  HourlyWeatherCard(),
+                  HourlyWeatherCard(),
+                  HourlyWeatherCard(),
+                  HourlyWeatherCard(),
+
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //ADDITIONAL INFORMATION
+            const Text(
+              "Additional Information",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Placeholder(
+              fallbackHeight: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HourlyWeatherCard extends StatelessWidget {
+  const HourlyWeatherCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      child: Card(
+        elevation: 6,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+
+          child: Column(
+            children: [
+              Text(
+                "9:00",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Icon(
+                Icons.cloud,
+                size: 20,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Rain",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
